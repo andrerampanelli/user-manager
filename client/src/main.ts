006bb667b50f1832ecf1f11c64e3f3844ac08192
André Vitor Rampanelli <andrerampanelli1@gmail.com>
2025-06-29 18:15:19 +0300

    refactor(client): remove commented-out VeeValidate setup code to clean up main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,9 +3,6 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-// VeeValidate setup imports (for later use)
-// import { configure } from 'vee-validate'
-// import { localize } from '@vee-validate/i18n'
 
 import App from './App.vue'
 import router from './router'
@@ -18,5 +15,4 @@ app.use(ElementPlus)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
-// VeeValidate global config can be added here later
 app.mount('#app')
